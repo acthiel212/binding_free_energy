5f6b52ed4e12853ae64f8321730066440a338809
minouemmad <minou-emmad@uiowa.edu>
2024-09-06 16:02:56 -0500

    user-defined flags

--- BindingFreeEnergy.py
+++ BindingFreeEnergy.py
@@ -1,14 +1,44 @@
+import argparse
 from openmm.app import *
 from openmm import *
 from openmm.unit import *
 from sys import stdout
 
-pdb = PDBFile('g1.pdb_2')
-forcefield = ForceField('g1.xml')
-system = forcefield.createSystem(pdb.topology, nonbondedMethod=NoCutoff, nonbondedCutoff=10*nanometer, constraints=None)
-vdwLambda = 0.5
-alchemicalAtoms = range(0, 2)
+# Argument parser for user-defined flags
+parser = argparse.ArgumentParser(description='OpenMM Alchemical Simulation with Custom Flags')
+parser.add_argument('--pdb_file', required=True, type=str, help='PDB file for the simulation')
+parser.add_argument('--forcefield_file', required=True, type=str, help='Force field XML file')
+parser.add_argument('--nonbonded_method', required=True, type=str, help='Nonbonded method: NoCutoff, CutoffNonPeriodic, PME, etc.')
+parser.add_argument('--nonbonded_cutoff', required=False, type=float, help='Nonbonded cutoff in nm (default: 1.0 nm)', default=1.0)
+parser.add_argument('--vdw_lambda', required=False, type=float, help='Value for van der Waals lambda (default: 0.5)', default=0.5)
+parser.add_argument('--alchemical_atoms', required=True, type=str, help='Range of alchemical atoms (e.g., "0,2")')
 
+args = parser.parse_args()
+
+# Parse alchemical_atoms input
+alchemical_atoms = range(*map(int, args.alchemical_atoms.split(',')))
+
+# Define flags based on user input
+pdb_file = args.pdb_file
+forcefield_file = args.forcefield_file
+nonbonded_cutoff = args.nonbonded_cutoff * nanometer
+vdw_lambda = args.vdw_lambda
+
+# Convert nonbonded_method string to OpenMM constant
+nonbonded_method_map = {
+    'NoCutoff': NoCutoff,
+    'CutoffNonPeriodic': CutoffNonPeriodic,
+    'PME': PME,
+    'Ewald': Ewald
+}
+nonbonded_method = nonbonded_method_map.get(args.nonbonded_method, NoCutoff)
+
+# Load PDB and Force Field
+pdb = PDBFile(pdb_file)
+forcefield = ForceField(forcefield_file)
+system = forcefield.createSystem(pdb.topology, nonbondedMethod=nonbonded_method, nonbondedCutoff=nonbonded_cutoff, constraints=None)
+
+# Setup simulation context
 numForces = system.getNumForces()
 forceDict = {}
 for i in range(numForces):
@@ -23,17 +53,13 @@ platform = Platform.getPlatformByName('CUDA')
 context = Context(system, integrator, platform)
 context.setPositions(pdb.getPositions())
 context.setVelocitiesToTemperature(300*kelvin)
-context.setParameter("AmoebaVdwLambda", vdwLambda)
+context.setParameter("AmoebaVdwLambda", vdw_lambda)
 state = context.getState(getEnergy=True, getPositions=True)
 print(state.getPotentialEnergy().in_units_of(kilocalories_per_mole))
 
-#simulation = Simulation(pdb.topology, system, integrator, platform)
-#simulation.context.setPositions(pdb.positions)
-
-## Annihilate == 2
-## Decouple == 1
-vdwForce.setAlchemicalMethod(2)
-for atomi in alchemicalAtoms:
+# Set alchemical method and parameters for van der Waals force
+vdwForce.setAlchemicalMethod(2)  # 2 == Annihilate, 1 == Decouple
+for atomi in alchemical_atoms:
     [parent, sigma, eps, redFactor, isAlchemical, type] = vdwForce.getParticleParameters(atomi)
     vdwForce.setParticleParameters(atomi, parent, sigma, eps, redFactor, True, type)
 
@@ -42,10 +68,3 @@ context.reinitialize(preserveState=True)
 print(context.getParameter("AmoebaVdwLambda"))
 state = context.getState(getEnergy=True, getPositions=True)
 print(state.getPotentialEnergy().in_units_of(kilocalories_per_mole))
-
-
-#simulation.reporters.append(PDBReporter('output.pdb', 1000))
-#simulation.reporters.append(StateDataReporter(stdout, 1000, step=True, kineticEnergy=True, potentialEnergy=True, totalEnergy=True, temperature=True, separator=', '))
-#simulation.step(3000)
-
-
