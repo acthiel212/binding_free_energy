319bd6a76a07d3e40da8c0b37c877f34f8a2c012
Minou Emmad <minou-emmad@uiowa.edu>
2024-09-09 17:10:06 -0500

    Update BindingFreeEnergy.py

--- BindingFreeEnergy.py
+++ BindingFreeEnergy.py
@@ -10,7 +10,7 @@ parser.add_argument('--pdb_file', required=True, type=str, help='PDB file for th
 parser.add_argument('--forcefield_file', required=True, type=str, help='Force field XML file')
 parser.add_argument('--nonbonded_method', required=True, type=str, help='Nonbonded method: NoCutoff, CutoffNonPeriodic, PME, etc.')
 parser.add_argument('--num_steps', required=False, type=int, help='Number of MD steps to take.', default=1000)
-parser.add_argument('--step_size', required=False, type=int, help='Step size given to integrator in fs.', default=2)
+parser.add_argument('--step_size', required=False, type=int, help='Step size given to integrator in fs.', default=1)
 parser.add_argument('--nonbonded_cutoff', required=False, type=float, help='Nonbonded cutoff in nm (default: 1.0 nm)', default=1.0)
 parser.add_argument('--vdw_lambda', required=False, type=float, help='Value for van der Waals lambda (default: 1.0)', default=1.0)
 parser.add_argument('--elec_lambda', required=False, type=float, help='Value for electrostatic lambda (default: 0.0)', default=0.0)
