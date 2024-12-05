f811f0000df568333f29dd5a909ff463c814d914
acthiel212 <acthiel212@gmail.com>
2024-12-05 17:08:36 -0600

    Added capability to calculate a subsection of the guest to use in COM restraints. This calculation is done by a selecting atoms in the guest that are under a specified distance cutoff from the host COM.

--- alchemistry/Harmonic_Restraint.py
+++ alchemistry/Harmonic_Restraint.py
@@ -1,5 +1,10 @@
 from openmm import *
+import subprocess
 from intspan import intspan
+import re
+
+path_to_file = os.path.dirname(os.path.abspath(__file__))
+
 def create_restraint(restraint_atoms_1, restraint_atoms_2, restraint_constant, restraint_lower_distance, restraint_upper_distance):
     restraint_atoms_1 = list(intspan(restraint_atoms_1))
     restraint_atoms_2 = list(intspan(restraint_atoms_2))
@@ -20,3 +25,19 @@ def create_restraint(restraint_atoms_1, restraint_atoms_2, restraint_constant, r
                                restraint_upper_distance * openmm.NmPerAngstrom])
 
     return restraint
+
+def calculate_restraint_subsection(host_guest_file_path, cutoff):
+    with open('RestrainGuest.log', "w") as outfile:
+        subprocess.run([f"{path_to_file}/../ffx-1.0.0/bin/ffxc", "test.RestrainGuest", "--distanceCutoff",f"{cutoff}",
+                        f"{host_guest_file_path}"], stdout=outfile, text=True)
+
+    with open('RestrainGuest.log', 'r') as infile:
+        for line in infile:
+            if re.search('Restrain list indices:', line):
+                #Grab just the list indices
+                indexString = line.split(':')[1]
+                # Remove brackets and whitespace
+                indices = indexString.replace('[', '').replace(']', '').replace(' ','')
+    print(f"Atom indices of guest within specified cutoff of {cutoff} Angstroms that will be restrained:", indices)
+
+    return indices
